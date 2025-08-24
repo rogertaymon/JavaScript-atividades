777ecd66f03e4cd4c88b0b4439a1a6ae30a9830e
Roger Duarte <rogertaymon200@gmail.com>
2025-08-24 19:58:52 -0300

    Envio atividade01

--- atv2.js
+++ atv2.js
@@ -1,7 +1,8 @@
-let nota1 =10.0
-let nota2 =8.0
-let nota3 =7.0
-let nota4 =7.5
+const leia = require ('readline-sync');
+let nota1 =leia.questionFloat('Digite a primeira nota: ');
+let nota2 =leia.questionFloat('Digite a segunda nota: ');
+let nota3 =leia.questionFloat('Digite a terceira nota: ');
+let nota4 =leia.questionFloat('Digite a quarta nota: '); 
 resultado = (nota1 + nota2 + nota3 + nota4) / 4
 console.log('A media das notas é ' + resultado)
 
